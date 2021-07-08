46aab94693b22500131971d19996b52f67877157
Noah Cordova <mrcordova@ucdavis.edu>
2021-07-07 20:05:14 -0700

    intiial

---
+++ src/components/common/like.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+// Input: liked: boolean
+//Output: onClick
+
+const Like = ({liked, onClick}) => {
+  
+        let classes = "fa fa-heart";
+        if (!liked) classes += "-o";
+        return (
+          <i
+            onClick={onClick}
+            style={{ cursor: "pointer" }}
+            className={classes}
+            aria-hidden="true"
+          ></i>
+        );
+        
+};
+ 
+export default Like;
+
+
